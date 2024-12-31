5dccd973503c88968106642076b26ccb97e83465
Payn <ivanttt54@gmail.com>
2024-12-30 21:19:12 -0300

    refactor on day2

--- src/day2.zig
+++ src/day2.zig
@@ -2,67 +2,23 @@ const std = @import("std");
 const common = @import("common.zig");
 
 pub fn solve(allocator: std.mem.Allocator) !void {
-    try solvePartOne(allocator);
-    try solvePartTwo(allocator);
-}
-
-fn solvePartOne(allocator: std.mem.Allocator) !void {
     const input_buffer = try common.loadInput("inputs/day2.txt", allocator);
 
-    var safe_reports: u32 = 0;
+    try solvePartOne(input_buffer);
+    try solvePartTwo(input_buffer);
+}
 
-    var line_iterator = std.mem.splitAny(u8, input_buffer, "\n");
-    while (line_iterator.next()) |line| {
-        var is_report_safe = true;
-        var report_order = ReportOrder.none;
-        var previuous_level: i32 = 0;
-
-        var space_iterator = std.mem.splitAny(u8, line, " ");
-        while (space_iterator.next()) |level_str| {
-            if (level_str.len != 0) {
-                const level = try std.fmt.parseInt(i32, level_str, 0);
-                defer previuous_level = level;
-
-                if (previuous_level != 0) {
-                    // check difference between levels
-                    const diff = @abs(level - previuous_level);
-                    if (diff > 3 or diff < 1) {
-                        is_report_safe = false;
-                        break;
-                    }
-
-                    var order = ReportOrder.none;
-                    defer report_order = order;
-
-                    if (previuous_level < level) {
-                        order = .asceding;
-                    } else {
-                        order = .desceding;
-                    }
-
-                    // check the ordering between levels
-                    if (report_order != .none and report_order != order) {
-                        is_report_safe = false;
-                        break;
-                    }
-                }
-            } else {
-                is_report_safe = false;
-                break;
-            }
-        }
-
-        if (is_report_safe) {
-            safe_reports += 1;
-        }
-    }
-
+fn solvePartOne(input_buffer: []const u8) !void {
+    const safe_reports = try getSafeReports(input_buffer, 0);
     std.debug.print("Part 1 result: {d}\n", .{safe_reports});
 }
 
-fn solvePartTwo(allocator: std.mem.Allocator) !void {
-    const input_buffer = try common.loadInput("inputs/day2.txt", allocator);
+fn solvePartTwo(input_buffer: []const u8) !void {
+    const safe_reports = try getSafeReports(input_buffer, 1);
+    std.debug.print("Part 2 result: {d}\n", .{safe_reports});
+}
 
+fn getSafeReports(input_buffer: []const u8, tolerence: u32) !u32 {
     var safe_reports: u32 = 0;
 
     var line_iterator = std.mem.splitAny(u8, input_buffer, "\n");
@@ -103,13 +59,13 @@ fn solvePartTwo(allocator: std.mem.Allocator) !void {
             }
         }
 
-        // check if only has a single bad report
-        if (bad_levels <= 1) {
+        // check bad levels against the tolerence
+        if (bad_levels <= tolerence) {
             safe_reports += 1;
         }
     }
 
-    std.debug.print("Part 2 result: {d}\n", .{safe_reports});
+    return safe_reports;
 }
 
 const ReportOrder = enum {
