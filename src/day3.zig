45c319370b9efdbc779efd163cf8c6ea721ff251
Payn <ivanttt54@gmail.com>
2024-12-31 20:38:07 -0300

    day3 part 2

--- src/day3.zig
+++ src/day3.zig
@@ -5,6 +5,7 @@ pub fn solve(allocator: std.mem.Allocator) !void {
     const input_buffer = try common.loadInput("inputs/day3.txt", allocator);
 
     try solvePartOne(input_buffer);
+    try solvePartTwo(input_buffer);
 }
 
 fn solvePartOne(input_buffer: []const u8) !void {
@@ -41,6 +42,61 @@ fn solvePartOne(input_buffer: []const u8) !void {
     }
 
     std.debug.print("Part 1 result: {d}\n", .{result});
+}
+
+fn solvePartTwo(input_buffer: []const u8) !void {
+    var result: u32 = 0;
+
+    var instruction_str: [8]u8 = undefined;
+    var instruction_slice: []u8 = undefined;
+    var instruction_enabled = true;
+    for (input_buffer, 0..) |char, i| {
+        _ = char;
+
+        if (i > 6) {
+            @memset(&instruction_str, 0);
+            sizedMemcpy(&instruction_str, input_buffer, i - 6, 7);
+            instruction_slice = std.mem.span(@as([*:0]u8, @ptrCast(&instruction_str)));
+            if (std.mem.eql(u8, instruction_slice, "don't()")) {
+                instruction_enabled = false;
+            }
+
+            @memset(&instruction_str, 0);
+            sizedMemcpy(&instruction_str, input_buffer, i - 3, 4);
+            instruction_slice = std.mem.span(@as([*:0]u8, @ptrCast(&instruction_str)));
+            if (std.mem.eql(u8, instruction_slice, "do()")) {
+                instruction_enabled = true;
+            }
+        }
+
+        if (i > 3) {
+            @memset(&instruction_str, 0);
+            sizedMemcpy(&instruction_str, input_buffer, i - 3, 4);
+            instruction_slice = std.mem.span(@as([*:0]u8, @ptrCast(&instruction_str)));
+            if (std.mem.eql(u8, instruction_slice, "mul(") and instruction_enabled) {
+                var num1_str: [4]u8 = undefined;
+                var num2_str: [4]u8 = undefined;
+                @memset(&num1_str, 0);
+                @memset(&num2_str, 0);
+
+                var index = i;
+                const num1_valid = parseNumbers(&num1_str, input_buffer, &index, ',');
+                const num2_valid = parseNumbers(&num2_str, input_buffer, &index, ')');
+
+                if (num1_valid and num2_valid) {
+                    const num1_slice = std.mem.span(@as([*:0]u8, @ptrCast(&num1_str)));
+                    const num2_slice = std.mem.span(@as([*:0]u8, @ptrCast(&num2_str)));
+
+                    const num1 = try std.fmt.parseInt(u32, num1_slice, 10);
+                    const num2 = try std.fmt.parseInt(u32, num2_slice, 10);
+
+                    result += num1 * num2;
+                }
+            }
+        }
+    }
+
+    std.debug.print("Part 2 result: {d}\n", .{result});
 }
 
 fn parseNumbers(num: *[4]u8, buffer: []const u8, start: *usize, stop: u8) bool {
